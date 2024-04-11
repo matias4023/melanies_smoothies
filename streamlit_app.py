9ef277320f7e573624b0070cfa9fe40646151c5d
matias4023 <108098100+matias4023@users.noreply.github.com>
2024-04-11 15:49:20 -0500

    Update streamlit_app.py
    Comment unneeded expression

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list:
         ingredients_string += fruits_chosen + ' '
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruits_chosen,' is ', search_on, '.')
+        #st.write('The search value for ', fruits_chosen,' is ', search_on, '.')
 
         st.subheader(fruits_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
