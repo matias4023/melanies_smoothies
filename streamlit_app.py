6e8484970899bce7018607879183332a9858fdf6
matias4023 <108098100+matias4023@users.noreply.github.com>
2024-04-11 15:44:26 -0500

    Update streamlit_app.py
    Uning loc to identify search word

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert snowpark dataframe to pandas to use loc function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
@@ -35,6 +35,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruits_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
